c0df7bddb7ab4d7bd2fa5e425e7a36593413c133
Sam <stocki9000@gmail.com>
2019-02-09 23:59:08 +0000

    Added projects

---
+++ src/Controllers/AccountsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UserAccount.Models;
+
+namespace UserAccount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Account>> Get(string id)
+        {
+            return Ok(new Account() 
+            {
+                Id = id
+            });
+
+            // 200 - OK
+            // 404 - Not Found
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Account account)
+        {
+            return Created("", account);
+
+            // 201 - Created
+            // 400 - Bad Request
+            // 409 - Conflict
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(string id, [FromBody] Account account)
+        {
+            return Created("", account);
+
+            // 201 - Created
+            // 202 - Accepted
+            // 400 - Bad Request
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id)
+        {
+            return NoContent();
+            
+            // 204 - No Content
+        }
+    }
+}
---
+++ src/Models/Account.cs
@@ -0,0 +1,15 @@
+namespace UserAccount.Models
+{
+    public class Account
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
